bfb66a86f61107d450203c03722e083d2546b38b
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-01 13:24:39 -0700

    store and middleware setup

---
+++ src/store.js
@@ -0,0 +1,26 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import promiseMiddleware from './promise-middleware';
+import { error, loading } from './components/app/reducers';
+import { albums, images } from '../src/components/albums/reducers';
+
+const rootReducer = combineReducers({
+  error,
+  loading,
+  albums,
+  images
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(
+      thunk,
+      promiseMiddleware
+    )
+  )
+);
+
+export default store;
