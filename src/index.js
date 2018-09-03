704b4457864aa290069938e6137ae40166ca4d1a
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-03 13:05:38 -0700

    took out all redux features

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/Main.css';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import App from './components/app/App';
-import store from './store';
+// import store from './store';
 
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App/>
+//   </Provider>,
+//   document.getElementById('root')
+// );
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
+  
+  <App/>,
   document.getElementById('root')
 );
