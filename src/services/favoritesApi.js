48651747e6160c6c4d803d6ec8ff0d5377752bea
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-03 12:33:06 -0700

    got it to stage favorites.api

--- src/services/favoritesApi.js
+++ src/services/favoritesApi.js
@@ -1,9 +1,9 @@
 import { put, get, del } from './request';
 
-const URL = 'http://localhost:3000/DiscogsApi';
+const URL = 'http://localhost:3000/api';
 const FAVORITES_URL = `${URL}/favorites`;
 
-const getFavoriteUrl = artist_id => `${FAVORITES_URL}/artists/${artist_id}.json`;
+const getFavoriteUrl = id => `${FAVORITES_URL}/id-${id}.json`;
 
 export const addFavorite = ({ id, name, profile, images }) => {
   const url = getFavoriteUrl(id);
